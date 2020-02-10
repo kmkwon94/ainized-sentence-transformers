96bec525cf24b562c08737feac4d51ce04470d63
KangMin Kwon <kmkwon94@gmail.com>
2020-02-10 12:07:47 +0900

    modified main.py, upload swagger.yaml

--- application_semantic_search.py
+++ application_semantic_search.py
@@ -1,59 +1,39 @@
 """
 This is a simple application for sentence embeddings: semantic search
-
 We have a corpus with various sentences. Then, for a given query sentence,
 we want to find the most similar sentence in this corpus.
-
 This script outputs for various queries the top 5 most similar sentences in the corpus.
 """
 from sentence_transformers import SentenceTransformer
 import scipy.spatial
 
-# Query sentences:
-test_queries = [
 
-    # input query
-]
-
-
-def cleanTestQueries():
-    if len(test_queries) != 0:
-        for index_number in range(len(test_queries)):
-            del test_queries[index_number]
-
-
-def addQueries(input_query):
-    global test_queries
-    test_queries = []
-    for sentences in input_query:
-        test_queries.append(sentences)
-    return test_queries
-
-
-def runAnalysis():
+def runSample():
     embedder = SentenceTransformer('bert-base-nli-mean-tokens')
-    exampleSentencesFileRead = open(
-        '/ainized-sentence-transformers/upload/sentences.txt',
-        'r+',
-        encoding="utf-8")
-    # Corpus with example sentences
-    corpus = list(exampleSentencesFileRead)
-    corpus = [line.strip() for line in corpus]
 
+    # Corpus with example sentences
+    corpus = [
+        'A man is eating food.', 'A man is eating a piece of bread.',
+        'The girl is carrying a baby.', 'A man is riding a horse.',
+        'A woman is playing violin.',
+        'Two men pushed carts through the woods.',
+        'A man is riding a white horse on an enclosed ground.',
+        'A monkey is playing drums.', 'A cheetah is running behind its prey.'
+    ]
     corpus_embeddings = embedder.encode(corpus)
 
-    query_embeddings = embedder.encode(test_queries)
+    # Query sentences:
+    queries = [
+        'A man is eating pasta.',
+        'Someone in a gorilla costume is playing a set of drums.',
+        'A cheetah chases prey on across a field.'
+    ]
+    query_embeddings = embedder.encode(queries)
 
     # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
     closest_n = 5
-    real_results = {
-        # query: [
-        #     ('corpus1', 0.74),
-        #     ('corpus2', 0.32),
-        #     ('corpus3', 0.1),
-        # ]
-    }
-    for query, query_embedding in zip(test_queries, query_embeddings):
+    real_results = {}
+    for query, query_embedding in zip(queries, query_embeddings):
         distances = scipy.spatial.distance.cdist([query_embedding],
                                                  corpus_embeddings,
                                                  "cosine")[0]
@@ -67,11 +47,11 @@ def runAnalysis():
         real_results[query] = []
 
         for idx, distance in results[0:closest_n]:
-            print(corpus[idx], "(Score: %.4f)" % (1 - distance))
+            print(corpus[idx].strip(), "(Score: %.4f)" % (1 - distance))
             real_results[query].append((corpus[idx], 1 - distance))
 
     return real_results
 
 
-if __name__ == '__main__':
-    result_analysis = runAnalysis()
+if __name__ == "__main__":
+    result_analysis = runSample()
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from application_semantic_search_open_corpus import runAnalysis, addQueries
+from application_semantic_search import runSample
 app = Flask(__name__)
+CORS(app)
 
 
 # 업로드 HTML 렌더링
 @app.route('/')
 def render_file():
-    return render_template('upload.html')
+    return render_template('initial_check.html')
+
+
+@app.route('/initialCheck', methods=['GET', 'POST'])
+def initial_check():
+    check_value = request.form['checkValue']
+    if check_value == "0":
+        return redirect(url_for('sample_run'))
+    elif check_value == "1":
+        return render_template('upload.html')
+
+
+@app.route('/sample', methods=['GET', 'POST'])
+def sample_run():
+    sample_result = runSample()
+    return jsonify(sample_result)
 
 
 @app.route('/fileUpload', methods=['GET', 'POST'])
@@ -28,8 +46,13 @@ def render_query():
 
 @app.route('/changeQuery', methods=['GET', 'POST'])
 def change_query():
-    input_value = request.form['input_query']
-    addQueries(input_value)
+    split_input_value = []
+    input_value = []
+    input_value.append(request.form['input_query'])
+    for i in range(1):
+        split_input_value = input_value[i].split(",")
+    split_input_value = [line.strip() for line in split_input_value]
+    addQueries(split_input_value)
     return redirect(url_for('run_file'))
 
 
@@ -39,41 +62,13 @@ def run_file():
     return jsonify(result)
 
 
-if __name__ == '__main__':
-    # server execute
-    app.run(host='0.0.0.0', port=80, debug=True)
-
-
-"""
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from werkzeug.utils import secure_filename
-from application_semantic_search_open_corpus import runAnalysis
-app = Flask(__name__)
-
-
-# 업로드 HTML 렌더링
-@app.route('/')
-def render_file():
-    return render_template('upload.html')
-
-
-@app.route('/fileUpload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        # 저장할 경로 + 파일명
-        f.save('/ainized-sentence-transformers/upload/' +
-               secure_filename(f.filename))
-        return redirect(url_for('run_file'))  # redirect할 것을 method명으로 처리함
-
-
-@app.route('/runfile')
-def run_file():
-    result = runAnalysis()
-    return jsonify(result)
+@app.route('/upload_your_sample', methods=['GET', 'POST'])
+def upload_your_sample():
+    upload_file()
+    change_query()
+    return run_file()
 
 
 if __name__ == '__main__':
     # server execute
     app.run(host='0.0.0.0', port=80, debug=True)
-"""
